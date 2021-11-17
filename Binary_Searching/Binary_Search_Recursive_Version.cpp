1848c312fe4370c004dcbf550b3802cafd4e53c8
Erik_Bagratyan <erik.bagratyan2000@mail.ru>
2021-11-17 17:59:03 +0400

    Binary Search Recursive

--- Binary_Searching/Binary_Search_Recursive_Version.cpp
+++ Binary_Searching/Binary_Search_Recursive_Version.cpp
@@ -14,7 +14,7 @@ int Binary_Search_Recursive(const std::vector<int>& vec, int left, int right, in
     if(vec[mid] < number)
         return Binary_Search_Recursive(vec,mid + 1, right,number);
     else 
-        return Binary_Search_Recursive(vec,0, mid - 1,number);
+        return Binary_Search_Recursive(vec,left, mid - 1,number);
 }
 
 void Print_Array(const std::vector<int>& vec)
